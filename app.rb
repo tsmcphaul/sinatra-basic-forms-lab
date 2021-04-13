295e65a0e9512c7d422f7c55ef7cad0843232311
Tammarra McPhaul <shaemc22@gmail.com>
2021-04-12 20:55:25 -0400

    commit tree a462a41222f2003dcad11c8d46bec3d5eeb13830

--- app.rb
+++ app.rb
@@ -2,4 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+get '/' do
+    erb :index
+end
+
+get '/new' do
+    erb :create_puppy
+end
+
+post '/puppy' do
+    name = params[:name]
+    breed = params[:breed]
+    age = params[:age]
+    @puppy = Puppy.new(name, breed, age)
+    erb :display_puppy
+end
+
+
 end
